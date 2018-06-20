31dddaf6cc1b19989e5928f59e3156b9321f646d
Hackbright Student <no-reply@hackbrightacademy.com>
2018-06-20 12:37:35 -0700

    wrote add and subtract functions

--- arth.py
+++ arth.py
@@ -6,3 +6,8 @@ def add(num1, num2):
     x = num1 + num2
     return x
 
+
+def subtract(num1, num2):
+    """Return the second number subtracted from the first."""
+    y = num1 - num2
+    return y
