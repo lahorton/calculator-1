1b90b17b8f60a90fb3a7d2d9812e35d031fab344
Hackbright Student <no-reply@hackbrightacademy.com>
2018-06-20 12:39:23 -0700

    wrote multiply and divide functions

--- arth.py
+++ arth.py
@@ -11,3 +11,15 @@ def subtract(num1, num2):
     """Return the second number subtracted from the first."""
     y = num1 - num2
     return y
+
+
+def multiply(num1, num2):
+    """Multiply the two inputs together."""
+    a = num1 * num2
+    return a
+
+
+def divide(num1, num2):
+    """Divide the first input by the second and return the result."""
+    b = num1 / num2
+    return b
